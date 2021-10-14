4f46d948a626143cf266de4981ffb6955e789926
Carlyn Maw <carlynorama@gmail.com>
2021-10-14 11:26:15 -0700

    Text change for commit test

--- Sources/ImageHandler/ImageHandler.swift
+++ Sources/ImageHandler/ImageHandler.swift
@@ -1,5 +1,5 @@
 public struct ImageHandler {
-    public private(set) var text = "Hello, World!"
+    public private(set) var text = "Hello, Pretty World!"
 
     public init() {
     }
--- Tests/ImageHandlerTests/ImageHandlerTests.swift
+++ Tests/ImageHandlerTests/ImageHandlerTests.swift
@@ -6,6 +6,6 @@ final class ImageHandlerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ImageHandler().text, "Hello, World!")
+        XCTAssertEqual(ImageHandler().text, "Hello, Pretty World!")
     }
 }
