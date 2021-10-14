afad2aabc5ec348f7b34e1c495e6526a196636bb
carlynorama <carlynorama@gmail.com>
2021-10-14 15:18:28 -0700

    defaultImage

--- Sources/ImageHandler/ImageHandler.swift
+++ Sources/ImageHandler/ImageHandler.swift
@@ -1,6 +1,31 @@
+import UIKit
+import SwiftUI
+
+
 public struct ImageHandler {
+    
     public private(set) var text = "Hello, Very Pretty World!"
 
     public init() {
+    }
+    
+    static public var defaultImage:Image {
+        if let uiImage = ImageHandler.generateBlankImage(color: .blue) {
+            return Image(uiImage: uiImage)
+        } else {
+        return Image("photo")
+        }
+    }
+    
+    static private func generateBlankImage(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        guard let cgImage = image?.cgImage else { return nil }
+        return UIImage(cgImage: cgImage)
     }
 }
